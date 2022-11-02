6d57502762b482418c4520f03d8f79b127f35922
Viktoryia Supronenko <vika.supronenko@icloud.com>
2022-11-03 00:09:36 +0300

    Add project files.

---
+++ Less9/Collection.cs
@@ -0,0 +1,57 @@
+﻿namespace Less9
+{
+    class Collection<T>
+    {
+        T[] array;
+        public Collection()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] newArr = new T[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArr[i] = array[i];
+            }
+            newArr[array.Length] = item;
+            array = newArr;
+        }
+
+        public void Remove(T item)
+        {
+            int index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index > -1)
+            {
+                T[] newArr = new T[array.Length - 1];
+                for (int i = 0, j = 0; i < array.Length; i++)
+                {
+                    if (i == index) continue;
+                    newArr[j] = array[i];
+                    j++;
+                }
+                array = newArr;
+            }
+        }
+
+        public T GetItem(int index)
+        {
+            return array[index];
+        }
+
+        public int Count()
+        {
+            return array.Length;
+        }
+    }
+}
+
---
+++ Less9/Program.cs
@@ -0,0 +1,15 @@
+﻿using Less9;
+
+Collection<int> coll = new Collection<int>();
+coll.Add(1);
+coll.Add(2);
+coll.Add(3);
+coll.Add(4);
+coll.Add(5);
+coll.Remove(3);
+Console.WriteLine("Collection length: " + coll.Count());
+
+for (int i = 0; i < coll.Count(); i++)
+{
+    Console.WriteLine(coll.GetItem(i));
+}
